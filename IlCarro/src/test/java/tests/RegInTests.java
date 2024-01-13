7de160e7191bc57d4d57519068c48448ab4fe0b5
AlexKh1986 <alexkhenkin1986@gmail.com>
2024-01-13 19:42:54 +0200

    n

--- IlCarro/src/test/java/tests/RegInTests.java
+++ IlCarro/src/test/java/tests/RegInTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import models.User;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -48,46 +49,48 @@ public class RegInTests extends TestBase {
     public void regInDone() {
         int i = r.nextInt(1000);
         System.out.println(System.currentTimeMillis());
-        int z = (int) (System.currentTimeMillis() / 1000 % 3600);
+        int z = (int) (System.currentTimeMillis() / i % 3600);
         u.withFirstName("john")
                 .withLastName("snow")
                 .withEmail("snow" + z + "@gmail.com")
                 .withPass("SnowBall123!?");
         modelTestForRegIn();
         Assert.assertEquals(app.getHelperUser().getMessage(), "You are logged in success");
+        app.getHelperUser().clickOk();
+        app.getHelperUser().click(By.cssSelector (".navigation-link.ng-star-inserted[ng-reflect-query-params='[object Object]']"));
+        app.getDriver().navigate().refresh();
     }
 
     @Test
     public void registrationEmptyName() {
         int i = r.nextInt(1000);
         System.out.println(System.currentTimeMillis());
-        int z = (int) (System.currentTimeMillis() / 1000 % 3600);
+        int z = (int) (System.currentTimeMillis() / i % 3600);
         u.withFirstName("")
                 .withLastName("snow")
                 .withEmail("snow" + z + "@gmail.com")
                 .withPass("SnowBall123!?");
         modelTestForRegIn();
         Assert.assertFalse(app.getHelperUser().isLogged());
+        app.getDriver().navigate().refresh();
     }
 
     @Test
     public void registrationEmptyLastName() {
         int i = r.nextInt(1000);
         System.out.println(System.currentTimeMillis());
-        int z = (int) (System.currentTimeMillis() / 1000 % 3600);
+        int z = (int) (System.currentTimeMillis() / i % 3600);
         u.withFirstName("john")
                 .withLastName("")
                 .withEmail("snow" + z + "@gmail.com")
                 .withPass("SnowBall123!?");
         modelTestForRegIn();
         Assert.assertFalse(app.getHelperUser().isLogged());
+        app.getDriver().navigate().refresh();
     }
 
    @Test
    public void registrationWrongEmail() {
-//        int i = r.nextInt(1000);
-//        System.out.println(System.currentTimeMillis());
-//        int z = (int) (System.currentTimeMillis()/1000%3600);
         for (String invalidEmail : invalidEmails) {
             log.info("Testing failed "+invalidEmail);
             u.withFirstName("john")
@@ -108,7 +111,7 @@ public class RegInTests extends TestBase {
     public void registrationInvalidPass() {
         int i = r.nextInt(1000);
         System.out.println(System.currentTimeMillis());
-        int z = (int) (System.currentTimeMillis()/1000%3600);
+        int z = (int) (System.currentTimeMillis()/i%3600);
         for (String invalidPass : invalidPasswords) {
             log.info("Testing failed "+invalidPass);
             u.withFirstName("john")
